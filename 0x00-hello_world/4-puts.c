fc1aef6e43735ebe7832c7f837491bbdc65dc768
Sulaiman <D-Crony>
2023-03-19 14:00:23 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main . print out to stdout.
+ * Return: o if success.
+ */
+ int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

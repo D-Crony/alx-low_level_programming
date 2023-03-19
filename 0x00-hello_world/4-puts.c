ab277730ba4fa723779508ee7c178b47a0efee23
Sulaiman <D-Crony>
2023-03-19 14:10:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * Description: main . print out to stdout.
+ * Description: main . Prints out a string to stdout.
  * Return: o if success.
  */
- int main(void)
+ Int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);

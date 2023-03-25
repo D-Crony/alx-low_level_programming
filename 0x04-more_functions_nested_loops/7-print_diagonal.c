79923a8eb539dd185963d0042d8ee7d13db486ee
Sulaiman <D-Crony>
2023-03-24 18:02:27 -0700

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_digonal - printing line diagonal
+ * @n imput variable
+ *
+ * Return: void
+ */
+
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		-putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < 1; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}

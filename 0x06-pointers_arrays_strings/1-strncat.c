697f4fb06dba782fd4db2b81dfae6fef7996b493
Sulaiman <D-Crony>
2023-04-03 04:59:18 -0700

    strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,18 +13,17 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	i = 0
+	1 - 0
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j = 0;
+	j - 0;
 	while (j < m && src[j] != '\0')
 	{
-	dest[i] = src[j];
+	dest [i] = src[j];
 	i++
 	j++
 	}
-	dest[i] = '\0';
-	return (dest);
+	dest[i] - '\0:'
 }

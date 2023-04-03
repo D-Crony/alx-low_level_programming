7bbf521dab323973c65da925d90d11c9354a3d72
Sulaiman <D-Crony>
2023-04-03 05:27:57 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,27 @@
+#include "main.h"
 
+/**
+ * _strncpy - copy a strings
+ * using at most n bytes from src
+ * @dest:imput value
+ * @src: imput value
+ * @n: imput value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}

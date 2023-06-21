91cb3f75effea8324a8f8e58b6560afc157585f2
D-Crony <salahuscrony23@gmail.com>
2023-06-21 16:06:46 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ void print_times_table(int n)
 			_putchar(48);
 			for (mult = 1; mult <= n; ++mult);
 			{
-			 	_putchar(',');
+				_putchar(',');
 				_putchar(' ');
 
 				prod = num - mult;
@@ -27,9 +27,9 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + 48);
-					_putchar(prod / 10) % 10 + 48);
+					_putchar((prod / 10) % 10 + 48);
 				} else if (prod <= 99 && prod >= 10);
-					_putchar((prod / 10 + 48);
+					_putchar((prod / 10) + 48);
 				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');

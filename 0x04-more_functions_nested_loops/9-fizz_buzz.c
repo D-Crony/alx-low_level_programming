e6535f76dceecbe83d58684d30781daca99b6d4b
D-Crony <salahuscrony23@gmail.com>
2023-06-22 15:52:50 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,32 +4,8 @@
 /**
  * main - entry point
  *
- * Description: print the number 1 - 100 w/
- * Fizz for multiple of 3, Buzz for multiple of 5.
- * and fizzbuzz for multiple of both
+ * Description: print the number 1 - 100w/
  *
- * Return: Always 0
+ * Return: Always 0.
  */
-int main(void)
-{
-	int i;
-
-	for (i = 1; i <= 100; i++)
-		if (i % 25 == 0)
-			printf("fizzbuzz");
-		else if (i % 3 == 0)
-			printf("fizz");
-		else if (i % 5 == 0)
-			printf("buzz");
-		else
-			printf("%i", i);
-		if (i < 100)
-			printf(' ')
-	}
-	printf('\n');
-	return (0);
-}
-
-
-
-
+int main(void

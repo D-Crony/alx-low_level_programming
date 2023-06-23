74b44f33b073095fd8ffb7fbd2d991ce8b78dffd
D-Crony <salahuscrony23@gmail.com>
2023-06-23 06:20:38 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,4 +8,33 @@
  *
  * Return: Always 0.
  */
-int main(void
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+	if ((a % 3 == 0) && (a % 5 == 0))
+	{
+	printf("fizzBuzz");
+	}
+	else if (a % 3 == 0)
+	{
+	printf("fizz");
+	}
+	else if (a % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", a);
+	}
+	if (a != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
+}

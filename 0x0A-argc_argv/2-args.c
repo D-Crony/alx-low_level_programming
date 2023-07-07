fe4f79a8a8a1c8e7630bf10edec47f794db8c2a3
D-Crony <salahuscrony23@gmail.com>
2023-07-07 16:15:48 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - The agrs content to print
+ * @argv: Vector argument
+ * @argc: Count argument
+ *
+ * Return:0(Alway)
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+
+	return (0);
+}
